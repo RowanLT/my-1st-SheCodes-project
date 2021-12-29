088232bd2d92dad0a39f89ce6245768fa3f4e786
RowanLT <92042651+RowanLT@users.noreply.github.com>
2021-12-29 10:51:40 +0100

    Updated icon API in JS

--- src/script.js
+++ src/script.js
@@ -56,7 +56,7 @@ function clickSubmit(event) {
   let iconElement = document.querySelector("#icon");
   iconElement.setAttribute(
     "src",
-    "http://openweathermap.org/img/wn/04d@2x.png"
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
 }
 
